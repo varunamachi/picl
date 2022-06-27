00ac58a99777b881ae9f9e2ec688bc8c5a11fdb9
varunamachi <varunamachi@gmail.com>
2022-06-27 21:45:53 +0530

    Fixed script warnings

--- picl.sh
+++ picl.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 scriptDir="$(cd "$(dirname "$0")" || exit ; pwd -P)"
-root=$(readlink -f $scriptDir)
+root=$(readlink -f "$scriptDir")
 
 cmd="picl"
 cmdDir="${root}/cmd/${cmd}"
 if [ ! -d "$cmdDir" ] ; then
     echo "Command directory $cmdDir does not exist"
 fi
-cd "$cmdDir" || exit -1
+cd "$cmdDir" || exit 1
 echo "Building...."
 
-go build -ldflags "-s -w" -race -o "$root/_local/bin/picl" || exit -1
+go build -ldflags "-s -w" -race -o "$root/_local/bin/picl" || exit 1
 
 echo "Running...."
 echo
